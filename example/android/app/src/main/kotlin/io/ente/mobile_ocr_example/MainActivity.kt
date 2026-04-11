6be5afd3e4e2b79d455c18d3d02ec47ee07e5989
Anders Klint <anders.klint519@gmail.com>
2026-04-11 13:56:52 +0800

    Delete provided widgets by package and example app

--- example/android/app/src/main/kotlin/io/ente/mobile_ocr_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package io.ente.mobile_ocr_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/mobile_ocr.dart
+++ lib/mobile_ocr.dart
@@ -1,4 +1,2 @@
 export 'mobile_ocr_plugin.dart';
 export 'models/text_block.dart';
-export 'widgets/text_detector_widget.dart';
-export 'widgets/text_overlay_widget.dart';
